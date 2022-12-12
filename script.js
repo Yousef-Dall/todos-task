0e32e0a7381be6b8fcd4c46f04fb63c62e55780c
Yousef <longliveyousefdalloul@gmail.com>
2022-12-12 20:37:03 +0200

    change input focus

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
-
-
 const myData = { username: 'example' };
+fetch('https://jsonplaceholder.typicode.com/todos/1', {
+  method: 'POST', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(myData),
+})
+  .then((response) => response.json())
+  .then((myData) => {
+    console.log('Success:', myData);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 
-// fetch('https://jsonplaceholder.typicode.com/todos/1', {
-//   method: 'POST', 
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(myData),
-// })
-//   .then((response) => response.json())
-//   .then((myData) => {
-//     console.log('Success:', myData);
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//   });
 
-
-// fetch('https://jsonplaceholder.typicode.com/todos', {
-//   method: 'GET', 
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(myData),
-// })
-//   .then((response) => response.json())
-//   .then((myData) => {
-//     console.log('Success:', myData);
-//   })
-//   .catch((error) => {
-//     console.error('Error:', error);
-//   });
+fetch('https://jsonplaceholder.typicode.com/todos', {
+  method: 'GET', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(myData),
+})
+  .then((response) => response.json())
+  .then((myData) => {
+    console.log('Success:', myData);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 let x = 2
 const cardContainer = document.getElementById("section")
 const innp = document.getElementById("input-card")
@@ -40,17 +37,17 @@ const blala = document.getElementById("input-title")
 const add_btn = document.getElementById("addbt")
 const blalala = document.getElementById("btn")
 
-// for (let x = 0; x < 100; x++) {
-//   fetch(`https://jsonplaceholder.typicode.com/posts`)
-//   .then((response) => response.json())
-//   .then((data) => cardContainer.insertAdjacentHTML("beforeend", `<div class="card">
-//   <p class="card-title">${data[x].title}</p>
-//   <p class="card-text">
-//     ${data[x].body}
-//   </p>
-// </div>`));
+for (let x = 0; x < 100; x++) {
+  fetch(`https://jsonplaceholder.typicode.com/posts`)
+  .then((response) => response.json())
+  .then((data) => cardContainer.insertAdjacentHTML("beforeend", `<div class="card">
+  <p class="card-title">${data[x].title}</p>
+  <p class="card-text">
+    ${data[x].body}
+  </p>
+</div>`));
   
-// }
+}
 
 function hide() {
   innp.classList.toggle("hide")
@@ -90,8 +87,8 @@ add_btn.addEventListener("click", function(e) {
       add_btn.classList.remove("hide");
       blalala.classList.remove("rotate")
   });
-
-
-
-  
-
+  blala.addEventListener("keyup", function(e) {
+    if (e.which == 13) {
+      bla.focus()
+        }
+    });
